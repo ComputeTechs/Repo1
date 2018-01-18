c3c551647baa3f25cda72113e0f76508e943d400
ComputeTechs <ComputeTechs@gmail.com>
2018-01-18 14:05:55 -0600

    Errors Corrected as per Java compiler

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
-	public static void main(string[] args) {
-		for(int i=1;i=10;i++){
-			System.out.println("Hello world" +i);
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++){
+			System.out.println("Hello world..." +i);
 		}
 	}
 }
